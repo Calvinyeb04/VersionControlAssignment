11a01846fb3b730dcb62122c149ae36bdc4cb9ab
Calvi <astrocalvin8@gmail.com>
2023-10-31 14:15:50 -0400

    Add project files.

---
+++ VersionControlAssignment/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using VersionControlAssignment.Models;
+
+namespace VersionControlAssignment.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new { Name = "Calvin Yeboah" };
+            return Json(data);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ VersionControlAssignment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VersionControlAssignment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
