b3783dbdff8cf7e116209ae479d86b1f9e4e7cc5
Calvi <astrocalvin8@gmail.com>
2023-10-31 14:29:10 -0400

    .

---
+++ VersionControlAssignment/Controllers/HomeController2.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VersionControlAssignment.Controllers
+{
+    public class NameController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new { Name = "Calvin Yeboah" };
+            return Json(data);
+        }
+    }
+}
